a9b9e723e052109ae0ee41db10c1203304851923
Cecilia Zhou <lzhou192@student.fuhsd.org>
2026-04-22 22:38:57 -0700

    added mouse and keyboard input

--- src/breakout/Paddle.java
+++ src/breakout/Paddle.java
@@ -2,17 +2,27 @@ package breakout;
 
 import engine.Actor;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 
 public class Paddle extends Actor{
 
+	double dx;
 	public Paddle() {
 		String path = getClass().getClassLoader().getResource("breakoutresources/paddle.png").toString();
 		Image img = new Image(path);
 		this.setImage(img);
+		
+		dx = 4; 
 	}
 	@Override
 	public void act(long now) {
-		// TODO Auto-generated method stub
+		if (getWorld().isKeyPressed(KeyCode.LEFT)) {
+			move(-dx, 0);
+		}
+		
+		if (getWorld().isKeyPressed(KeyCode.RIGHT)) {
+			move(dx, 0);
+		}
 		
 	}
 	
