e7f26b01c567f9e2c63599c0cb9ece7c3c3d13df
Cecilia Zhou <lzhou192@student.fuhsd.org>
2026-04-23 00:11:19 -0700

    Update Brick.java

--- src/breakout/Brick.java
+++ src/breakout/Brick.java
@@ -1,5 +1,20 @@
 package breakout;
 
-public class Brick {
+import engine.Actor;
+import javafx.scene.image.Image;
+
+public class Brick extends Actor {
+
+	public Brick() {
+		String path = getClass().getClassLoader().getResource("breakoutresources/brick.png").toString();
+		Image img = new Image(path);
+		this.setImage(img);
+	}
+	
+	@Override
+	public void act(long now) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
